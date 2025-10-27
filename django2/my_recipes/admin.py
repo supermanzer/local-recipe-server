2069ff5a53d23929ccb7ebed211a70a048c0e6a5
supermanzer <ryan.manzer@gmail.com>
2025-10-27 06:36:59 -0700

    Adding API app

--- django2/my_recipes/admin.py
+++ django2/my_recipes/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ingredient, Recipe, RecipeIngredient, Step
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+    extra = 1
+
+
+class StepInline(admin.TabularInline):
+    model = Step
+
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ["name", "recipe_ingredients"]
+
+    inlines = [RecipeIngredientInline, StepInline]
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Step)
+class StepAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    pass
