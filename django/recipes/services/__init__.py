0ea234ccedf4cd492f348d348501bdf51ef9a960
supermanzer <ryan.manzer@gmail.com>
2025-11-22 10:23:30 -0800

    Fixes #1 - Removes outdated django service

--- django/recipes/services/__init__.py
+++
@@ -1,1 +0,0 @@
-# Make the services package available to the django app.
--- django/recipes/urls.py
+++
@@ -1,9 +0,0 @@
-"""
-Define URL routing for the recipes application
-"""
-
-from django.urls import path
-from recipes import views
-
-app_name = "recipes"
-urlpatterns = [path("", views.Index.as_view()), path("list", views.Recipes.as_view())]
