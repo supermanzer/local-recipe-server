01d641b406049bb05ecc678ac74001cbbdd6d9b8
supermanzer <ryan.manzer@gmail.com>
2026-01-03 09:20:15 -0800

    Adding shell script to automate dummy SSL certs

---
+++ init-ssl.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Configuration
+domains=(yourdomain.com www.yourdomain.com)
+rsa_key_size=4096
+data_path="./certbot"
+email="your-email@example.com" # Adding a valid email is required for Let's Encrypt
+staging=0 # Set to 1 if you're just testing to avoid hitting rate limits
+
+if [ -d "$data_path" ]; then
+  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+    exit
+  fi
+fi
+
+# 1. Download recommended TLS parameters
+if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
+  echo "### Downloading recommended TLS parameters ..."
+  mkdir -p "$data_path/conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+fi
+
+# 2. Create dummy certificate
+echo "### Creating dummy certificate for $domains ..."
+path="/etc/letsencrypt/live/$domains"
+mkdir -p "$data_path/conf/live/$domains"
+docker compose run --rm --entrypoint \
+  "openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
+    -keyout '$path/privkey.pem' \
+    -out '$path/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
+
+# 3. Start Nginx
+echo "### Starting nginx ..."
+docker compose up --force-recreate -d gateway
+
+# 4. Delete dummy certificate
+echo "### Deleting dummy certificate for $domains ..."
+docker compose run --rm --entrypoint \
+  "rm -rf /etc/letsencrypt/live/$domains /etc/letsencrypt/archive/$domains /etc/letsencrypt/renewal/$domains.conf" certbot
+
+# 5. Request real certificate
+echo "### Requesting Let's Encrypt certificate for $domains ..."
+# Join domains to -d args
+domain_args=""
+for domain in "${domains[@]}"; do
+  domain_args="$domain_args -d $domain"
+done
+
+# Select appropriate email arg
+case "$email" in
+  "") email_arg="--register-unsafely-without-email" ;;
+  *) email_arg="--email $email" ;;
+esac
+
+# Enable staging mode if needed
+if [ $staging != "0" ]; then staging_arg="--staging"; fi
+
+docker compose run --rm --entrypoint \
+  "certbot certonly --webroot -w /var/www/certbot \
+    $staging_arg \
+    $email_arg \
+    $domain_args \
+    --rsa-key-size $rsa_key_size \
+    --agree-tos \
+    --force-renewal" certbot
+
+# 6. Reload Nginx with real certificate
+echo "### Reloading nginx ..."
+docker compose exec gateway nginx -s reload
